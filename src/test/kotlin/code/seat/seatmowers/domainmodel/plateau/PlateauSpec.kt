8dabdb5dc03f42a875b8eb4cdbabe4240787b700
theUniC <theunic@gmail.com>
2022-12-30 18:22:24 +0100

    Added more tests

--- src/test/kotlin/code/seat/seatmowers/domainmodel/plateau/PlateauSpec.kt
+++ src/test/kotlin/code/seat/seatmowers/domainmodel/plateau/PlateauSpec.kt
@@ -4,21 +4,40 @@ import code.seat.seatmowers.application.command.CreatePlateau
 import org.axonframework.messaging.annotation.MessageHandlerInvocationException
 import org.axonframework.test.aggregate.AggregateTestFixture
 import org.axonframework.test.aggregate.FixtureConfiguration
-import org.axonframework.test.matchers.Matchers
+import org.junit.jupiter.api.Assertions
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.util.UUID
+
+private const val INVALID_COORDINATE_COMPONENT = 0
+private const val VALID_COORDINATE_COMPONENT = 15
 
 object PlateauSpec : Spek({
     describe("Plateau aggregate") {
         val fixture: FixtureConfiguration<Plateau> by memoized { AggregateTestFixture(Plateau::class.java) }
 
         describe("Creation") {
-            it("throws an exception when coordinates are not valid") {
+            it("throws an exception when X coordinate is not valid") {
                 fixture
                     .givenNoPriorActivity()
-                    .`when`(CreatePlateau(UUID.randomUUID(), 0, 15))
+                    .`when`(CreatePlateau(UUID.randomUUID(), INVALID_COORDINATE_COMPONENT, VALID_COORDINATE_COMPONENT))
                     .expectException(MessageHandlerInvocationException::class.java)
+            }
+
+            it("throws an exception when Y coordinate is not valid") {
+                fixture
+                    .givenNoPriorActivity()
+                    .`when`(CreatePlateau(UUID.randomUUID(), VALID_COORDINATE_COMPONENT, INVALID_COORDINATE_COMPONENT))
+                    .expectException(MessageHandlerInvocationException::class.java)
+            }
+
+            it("Creates a new plateau successfully") {
+                val id = UUID.randomUUID()
+                fixture
+                    .givenNoPriorActivity()
+                    .`when`(CreatePlateau(id, VALID_COORDINATE_COMPONENT, VALID_COORDINATE_COMPONENT))
+                    .expectSuccessfulHandlerExecution()
+                    .expectEvents(NewPlateauWasCreated(id, VALID_COORDINATE_COMPONENT, VALID_COORDINATE_COMPONENT))
             }
         }
     }
