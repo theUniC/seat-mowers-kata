16b8f7f37ea188bebf749d5175f66e27924ca151
theUniC <theunic@gmail.com>
2023-01-02 15:12:00 +0100

    Endpoint to move mower

---
+++ src/main/kotlin/code/seat/seatmowers/infrastructure/delivery/spring/controllers/PutMowerController.kt
@@ -0,0 +1,24 @@
+package code.seat.seatmowers.infrastructure.delivery.spring.controllers
+
+import code.seat.seatmowers.application.command.MoveRoverCommand
+import code.seat.seatmowers.infrastructure.delivery.spring.dtos.MowerMovementInputDto
+import org.axonframework.commandhandling.gateway.CommandGateway
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+import java.util.concurrent.Future
+
+@RestController
+class PutMowerController(val commandGateway: CommandGateway) {
+    @PutMapping("/plateaus/{plateauId}/mowers/{mowerId}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    fun handleRequest(@PathVariable plateauId: UUID, @PathVariable mowerId: UUID, @RequestBody mowerMovementInputDto: MowerMovementInputDto): Future<Unit> =
+        commandGateway
+            .send(MoveRoverCommand(plateauId, mowerId, mowerMovementInputDto.movement))
+}
---
+++ src/main/kotlin/code/seat/seatmowers/infrastructure/delivery/spring/dtos/MowerMovementInputDto.kt
@@ -0,0 +1,3 @@
+package code.seat.seatmowers.infrastructure.delivery.spring.dtos
+
+data class MowerMovementInputDto(val movement: String)
