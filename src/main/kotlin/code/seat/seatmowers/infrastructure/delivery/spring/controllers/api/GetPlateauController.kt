15a1d9a1a602614e0d72f8c1b9831a86a8cc8d1c
theUniC <theunic@gmail.com>
2023-01-06 21:31:07 +0100

    Added graphql queries

--- src/main/kotlin/code/seat/seatmowers/infrastructure/delivery/spring/controllers/api/GetPlateauController.kt
+++ src/main/kotlin/code/seat/seatmowers/infrastructure/delivery/spring/controllers/api/GetPlateauController.kt
@@ -11,12 +11,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.axonframework.messaging.responsetypes.ResponseTypes
 import org.axonframework.queryhandling.QueryGateway
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.hateoas.MediaTypes
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.Optional
 import java.util.UUID
 import java.util.concurrent.Future
 import kotlin.jvm.optionals.getOrElse
@@ -40,4 +43,10 @@ class GetPlateauController(val queryGateway: QueryGateway) {
                     linkTo(methodOn(GetPlateauMowersController::class.java).handleRequest(id)).withRel("mowers")
                 )
             }
+
+    @QueryMapping
+    fun plateau(@Argument id: UUID): Optional<PlateauOutputDto> =
+        queryGateway
+            .query(GetPlateauQuery(id), ResponseTypes.optionalInstanceOf(PlateauOutputDto::class.java))
+            .get()
 }
